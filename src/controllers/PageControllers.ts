8e0bc55ee45bab4590f005285ae60869835ae44c
devleonardorabelo <devleonardorabelo@gmail.com>
2020-12-13 18:37:46 -0300

    feat: added data from pages and posts

--- src/controllers/PageControllers.ts
+++ src/controllers/PageControllers.ts
@@ -1,7 +1,8 @@
 import { Request, Response } from 'express';
+import pages from '../data/pages';
 
 export = {
   async index(req: Request, res: Response) {
-    return res.json('pages');
+    return res.json(pages);
   },
 };
--- src/controllers/PostControllers.ts
+++ src/controllers/PostControllers.ts
@@ -1,7 +1,8 @@
 import { Request, Response } from 'express';
+import posts from '../data/posts';
 
 export = {
   async index(req: Request, res: Response) {
-    return res.json('posts');
+    return res.json(posts);
   },
 };
